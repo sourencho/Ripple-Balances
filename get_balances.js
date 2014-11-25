23c37acb63eb7816afb74cc336b69ea91b44b021
sourenp <souren@papazian.info>
2014-11-25 12:09:22 -0500

    fixed indentation

--- get_balances.js
+++ get_balances.js
@@ -5,42 +5,40 @@ var args = process.argv.slice(2);
 var Remote = require('ripple-lib').Remote;
 
 var remote = new Remote({
-  // see the API Reference for available options
-  servers: [ 'wss://s1.ripple.com:443' ]
+    // see the API Reference for available options
+    servers: [ 'wss://s1.ripple.com:443' ]
 });
 
 var options = {
-  account: 'rBTC1BgnrukYh9gfE8uL5dqnPCfZXUxiho',
-  ledger: 'validated'
+    account: 'rBTC1BgnrukYh9gfE8uL5dqnPCfZXUxiho',
+    ledger: 'validated'
 };
 
 remote.connect(function() {
     /* remote connected */
     remote.requestServerInfo(function(error, info) {
         var request = remote.requestAccountInfo(options, function(error, info) {
-            if (error) console.log(error);
-            else
-            {
-                main(args);
-            }
+                if (error) console.log(error);
+                else
+                {
+                        main(args);
+                }
         });
     });
 });
 
 function main(args) {
-
     accounts = get_accounts_info(args);
     //total_sums = calculate_totals(accounts);
 
     //print_accounts_info(accounts);
     //print_totals(total_sums);
-
 }
 
 function get_accounts_info(args) {
     accounts = {}
     async.each(account, get_account_info.bind(null, accounts), function(error) {
-        if(error) callback(error);
-        else callback(null, accounts);
+            if(error) callback(error);
+            else callback(null, accounts);
     });
 }
