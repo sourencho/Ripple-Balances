67edf3c1fee4bdce00eb9b3ddcbdb3f452ae5526
sourenp <souren@papazian.info>
2014-11-25 12:14:15 -0500

    wrote stucture of get_account_info

--- get_balances.js
+++ get_balances.js
@@ -27,18 +27,35 @@ remote.connect(function() {
     });
 });
 
-function main(args) {
-    accounts = get_accounts_info(args);
+function main(account_addresses) {
+    accounts = get_accounts_info(account_addresses);
     //total_sums = calculate_totals(accounts);
 
     //print_accounts_info(accounts);
     //print_totals(total_sums);
 }
 
-function get_accounts_info(args) {
+function get_accounts_info(account_addresses) {
     accounts = {}
-    async.each(account, get_account_info.bind(null, accounts), function(error) {
+    async.each(account_addresses, get_account_info.bind(null, accounts), function(error) {
             if(error) callback(error);
             else callback(null, accounts);
+    });
+}
+
+function get_account_info(accounts, curr_account, cb_acc_info) {
+    accounts[curr_account] = {};
+    async.parallel([
+        // Get account XRP balance
+        function(callback) {
+            get_acc_bal(curr_account, assign_acc_bal.bind(null, accounts, curr_account, callback));
+        }//,
+        // Get account lines
+        //function(callback) {
+        //    get_acc_lines(curr_account, assign_acc_lines.bind(null, accounts, curr_account, callback));
+        //}
+    ], function (error) {
+        if(error) cb_acc_info(error);
+        else cb_acc_info();
     });
 }
